8eba5f5aaa2ae5180a42d59310384164339edfd3
wardnath <wardnath.alt@gmail.com>
2024-07-05 20:33:47 -0400

    wip

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Shpool - Persistent Shell Session Tool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+        rust = pkgs.latest.rustChannels.stable.rust;
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "shpool";
+          version = "0.1.0";
+
+          src = ./.;
+
+          buildInputs = [ rust.cargo ];
+
+          cargoSha256 = pkgs.lib.fakeSha256;
+
+          buildPhase = ''
+            cargo build --release
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/shpool $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Persistent shell session tool";
+            license = licenses.mit;
+            maintainers = [ maintainers.yourName ];
+          };
+        };
+      });
+}
